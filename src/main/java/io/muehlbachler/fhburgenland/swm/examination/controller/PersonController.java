a39c052d9bbe024b7214c2e96509146b6cd39e22
mrtndwrk <martin.dworak1997@gmail.com>
2024-02-06 19:14:55 +0100

    refactor: fixed checkstyle warnings in PersonController relates to #17

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonController.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonController.java
@@ -1,7 +1,6 @@
 package io.muehlbachler.fhburgenland.swm.examination.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 import io.muehlbachler.fhburgenland.swm.examination.model.Person;
 import io.muehlbachler.fhburgenland.swm.examination.service.PersonService;
@@ -19,6 +17,10 @@ import io.muehlbachler.fhburgenland.swm.examination.service.PersonService;
 @RestController
 @RequestMapping("person")
 public class PersonController {
+
+    /**
+     * Service for managing Person entities.
+     */
     @Autowired
     private PersonService personService;
 
@@ -62,7 +64,8 @@ public class PersonController {
      * @return List of Persons matching the provided first and last names.
      */
     @GetMapping("/query")
-    public List<Person> query(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+    public List<Person> query(@RequestParam("firstName") String firstName,
+                              @RequestParam("lastName") String lastName) {
         return personService.findByName(firstName, lastName);
     }
 
